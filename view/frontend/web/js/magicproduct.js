286e6c9a665100c295f61ec81962bf21ee1b322b
Alex Dong <support@alothemes.com>
2021-02-24 23:26:16 +0700

    Fixed vertical slider Cumulative Layout Shift

--- view/frontend/web/js/magicproduct.js
+++ view/frontend/web/js/magicproduct.js
@@ -137,7 +137,7 @@ define([
 				var gridResponsive = [];
 				$.each( responsive, function( key, value ) { 
 					var breakpoint = {};
-					breakpoint[value.breakpoint] = parseInt(value.settings.slidesToShow);
+					breakpoint[value.breakpoint] = options.vertical ? parseInt(options.rows) : parseInt(value.settings.slidesToShow);
 					gridResponsive.push(breakpoint);
 				 });
 				var girdOptions = Object.assign({}, options);
