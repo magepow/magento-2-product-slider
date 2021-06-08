2fbfcd1e69af6c47dd1b831a2265243c5e0055f2
Alex Dong <team.magiccart@gmail.com>
2021-06-08 11:19:55 +0700

    Add sold

---
+++ Helper/Sold.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Magiccart 
+ * @category    Magiccart 
+ * @copyright   Copyright (c) 2014 Magiccart (http://www.alothemes.com/) 
+ * @license     http://www.alothemes.com/license-agreement.html
+ * @Author: DOng NGuyen<nguyen@dvn.com>
+ * @@Create Date: 2015-12-14 20:26:27
+ * @@Modify Date: 2020-07-17 16:44:21
+ * @@Function:
+ */
+
+namespace Magiccart\Magicproduct\Helper;
+
+class Sold extends Data
+{
+    public function getSoldQty($product)
+    {
+
+    }
+
+    public function getProductQty($_product)
+    {
+
+    }
+
+}
