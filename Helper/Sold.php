288716ff64fa026eb14c1a84becd3811443f07eb
Alex Dong <team.magiccart@gmail.com>
2021-06-08 11:21:35 +0700

    Update Sold.php

--- Helper/Sold.php
+++ Helper/Sold.php
@@ -12,16 +12,82 @@
 
 namespace Magiccart\Magicproduct\Helper;
 
-class Sold extends Data
+class Sold extends \Magento\Framework\App\Helper\AbstractHelper
 {
+
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime\TimezoneInterface
+     */
+    protected $_localeDate;
+
+    protected $_reportCollectionFactory;
+    protected $stockRegistry;
+
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        // \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
+        \Magento\Reports\Model\ResourceModel\Product\Sold\CollectionFactory  $reportCollectionFactory,
+        \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry
+    ) {
+        // $this->_localeDate = $localeDate;
+        $this->_reportCollectionFactory = $reportCollectionFactory;
+        $this->stockRegistry = $stockRegistry;
+        parent::__construct($context);
+    }
+
     public function getSoldQty($product)
     {
+        $productId = $product->getId();
+        $from   = $product->getData('special_from_date');
+        $to     = $product->getData('special_to_date');
 
+        // $todayStartOfDayDate = $this->_localeDate->date()->setTime(0, 0, 0)->format('Y-m-d H:i:s');
+        // $todayEndOfDayDate = $this->_localeDate->date()->setTime(23, 59, 59)->format('Y-m-d H:i:s');
+
+        $SoldProducts= $this->_reportCollectionFactory->create();
+        // $SoldProdudctCOl=$SoldProducts->addOrderedQty()
+        $SoldProdudctCOl=$SoldProducts->addOrderedQty($from, $to)
+        ->addAttributeToFilter('product_id', $productId);
+
+        $sold = 0;
+            if($SoldProdudctCOl->count()){
+            foreach ($SoldProdudctCOl as $item) {
+                $sold += (int) $item->getData('ordered_qty');
+            }
+        }
+        // echo $SoldProdudctCOl->getSelect()->__toString(); // echo sql
+
+        return $sold;
     }
 
     public function getProductQty($_product)
     {
-
+        $qty = 0;
+        $type = $_product->getTypeId();
+        $simpleType = [
+            'simple',
+            'virtual',
+            'downloadable'
+        ];
+        if(in_array($type, $simpleType)){
+            $stockItem = $_product->getExtensionAttributes()->getStockItem();
+            if(!$stockItem){
+                $stockItem = $this->stockRegistry->getStockItem($_product->getId(), $_product->getStore()->getWebsiteId());
+            }
+            $qty       = (int) $stockItem->getQty();
+            return $qty;
+        }
+        $allProducts = $_product->getTypeInstance()->getUsedProducts($_product, null);
+        foreach ($allProducts as $product) {
+            if ($product->isSaleable()) {
+                $stockItem = $this->stockRegistry->getStockItem(
+                        $product->getId(),
+                        $product->getStore()->getWebsiteId()
+                    );
+                $qty    += (int) $stockItem->getQty();
+            }
+        }
+        return $qty;
     }
 
 }
